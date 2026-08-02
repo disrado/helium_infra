7b42bfd9aae74ae2575b49e3d75132a38eec29fc
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-02 18:14:24 +0300

    add missing dependencies for sdl3 configuration step

--- build_env/linux/bootstrap.sh
+++ build_env/linux/bootstrap.sh
@@ -10,11 +10,17 @@ docker build -t helium-linux-build-env:latest .
 
 docker build -t helium-linux-jenkins-agent:latest ../../jenkins_agent/linux
 
+DOCKER_GID="$(getent group docker | cut -d: -f3)"
+
+sudo mkdir -p /home/jenkins/agent
+sudo chown -R 1000:1000 /home/jenkins/agent
+
 docker run -d --name "$JENKINS_AGENT_NAME" --restart unless-stopped \
+  --group-add "$DOCKER_GID" \
   -e JENKINS_URL="$JENKINS_URL" \
   -e JENKINS_SECRET="$JENKINS_SECRET" \
   -e JENKINS_AGENT_NAME="$JENKINS_AGENT_NAME" \
   -e JENKINS_WEB_SOCKET=true \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v jenkins_agent_workdir:/home/jenkins/agent \
+  -v /home/jenkins/agent:/home/jenkins/agent \
   helium-linux-jenkins-agent:latest
