457eb9f38cd294de6cb37ee45b19b936f956519c
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-09 01:03:33 +0300

    reflect project structure changes in job commands

--- vars/test.groovy
+++ vars/test.groovy
@@ -2,14 +2,12 @@ def call() {
     parallel(
         wsl: {
             node('wsl') {
-                runInContainer('build/linux-release/engine/helium_test_suite')
-                runInContainer('build/linux-release/game/game_test_suite')
+                runInContainer('build/linux-release/src/helium_test_suite')
             }
         },
         windows: {
             node('windows') {
-                bat 'build\\win-debug\\engine\\helium_test_suite.exe'
-                bat 'build\\win-debug\\game\\game_test_suite.exe'
+                bat 'build\\win-debug\\src\\helium_test_suite.exe'
             }
         }
     )
