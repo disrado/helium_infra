2ff0474c68daa470453f62816a2af5b555fbd6b1
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-10 23:50:01 +0300

    implement build job

--- vars/configure.groovy
+++ vars/configure.groovy
@@ -1,6 +1,12 @@
-def call(platform) {
-    checkout([$class: 'GitSCM', branches: scm.branches, userRemoteConfigs: scm.userRemoteConfigs,
-              extensions: [[$class: 'SubmoduleOption', recursiveSubmodules: true, parentCredentials: true]]])
+def call(platform, branch = null) {
+    if (branch) {
+        checkout([$class: 'GitSCM', branches: [[name: "*/${branch}"]],
+                  userRemoteConfigs: [[url: 'https://github.com/disrado/helium.git', credentialsId: 'helium_github_app']],
+                  extensions: [[$class: 'SubmoduleOption', recursiveSubmodules: true, parentCredentials: true]]])
+    } else {
+        checkout([$class: 'GitSCM', branches: scm.branches, userRemoteConfigs: scm.userRemoteConfigs,
+                  extensions: [[$class: 'SubmoduleOption', recursiveSubmodules: true, parentCredentials: true]]])
+    }
     if (platform == 'linux') {
         runInContainer('cmake --preset linux-release')
     } else {
