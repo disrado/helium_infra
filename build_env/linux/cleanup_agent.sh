87c38a8ac521c835c5328de538468a1f4be6ee9d
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-10 03:06:32 +0300

    fix scripts naming, make docker image prune less aggressive

--- build_env/linux/cleanup_agent.sh
+++ build_env/linux/cleanup_agent.sh
@@ -11,4 +11,4 @@ done
 # Purge stuck cleanWs() quarantine dirs.
 find "$workspaces_root" -maxdepth 1 -name '*_ws-cleanup_*' -exec rm -rf {} +
 
-docker system prune -af --volumes
+docker image prune -f
