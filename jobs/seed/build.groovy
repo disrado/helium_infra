637dcc8a5e174a870a8f62f4c4c77f71e5e2a143
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-12 23:24:23 +0300

    add names to remotes in build job to resolve checkout target

--- jobs/seed/build.groovy
+++ jobs/seed/build.groovy
@@ -4,13 +4,16 @@ pipelineJob('build') {
             scm {
                 git {
                     remote {
+                        name('infra')
                         url('https://github.com/disrado/helium_infra.git')
                         credentials('helium_github_app')
                     }
                     remote {
+                        name('game')
                         url('https://github.com/disrado/helium.git')
                         credentials('helium_github_app')
                     }
+                    branch('infra/main')
                 }
             }
             scriptPath('jobs/build/Jenkinsfile')
