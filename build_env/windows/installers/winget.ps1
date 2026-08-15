5dd336c4a8ee73afa7b49e895c5ea30de3371f82
mykyta.maltsev <n.maltsev.dis@gmail.com>
2026-08-16 02:37:49 +0300

    fix PATH refresh when winget package already installed

--- build_env/windows/installers/winget.ps1
+++ build_env/windows/installers/winget.ps1
@@ -4,7 +4,10 @@ function Install-WingetTool
 {
     param($Id, $Version, $Location, $Marker)
     $installed = winget list --exact --id $Id --accept-source-agreements 2>$null
-    if ((Test-Path $Marker) -and ($installed -match [regex]::Escape($Version))) { return }
+    if ((Test-Path $Marker) -and ($installed -match [regex]::Escape($Version))) {
+        Add-ToMachinePath -Marker $Marker
+        return
+    }
     winget install --exact --id $Id --version $Version --location $Location --scope machine --silent --accept-package-agreements --accept-source-agreements --disable-interactivity
     if ($LASTEXITCODE -ne 0) { throw "winget install failed for $Id (exit $LASTEXITCODE)" }
     Add-ToMachinePath -Marker $Marker
